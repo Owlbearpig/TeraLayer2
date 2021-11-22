b9463d6f9d492c86c642d6fb0ccce6fd83d987bc
Alex <Alexj07@gmail.com>
2021-11-22 20:16:21 +0100

    -0

---
+++ Visualizing/sliceplot.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""
+1. calculate sum(residuals) over 3D grid with some resolution(rez)
+2. 2D plot slices for different z set with slider
+"""
+
+
