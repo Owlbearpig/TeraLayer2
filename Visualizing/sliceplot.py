f7b7dd54b88b3bc67b459108d2218a7a2a209dd7
Owlbearpig <alexj07@gmail.com>
2021-11-25 00:59:23 +0100

    - hmmm, global minimum is the wrong minimum ...

--- Visualizing/sliceplot.py
+++ Visualizing/sliceplot.py
@@ -14,7 +14,7 @@ from matplotlib.widgets import Slider, Button
 lam, R = format_data(mask=default_mask)
 
 # should be resolution of axes d1, d2, d3
-rez_x, rez_y, rez_z = 1000, 1000, 1000
+rez_x, rez_y, rez_z = 200, 200, 200
 
 lb = array([0.000001, 0.00001, 0.000001])
 ub = array([0.001, 0.001, 0.001])
@@ -24,19 +24,19 @@ grd_x = np.linspace(lb[0], ub[0], rez_x)
 grd_y = np.linspace(lb[1], ub[1], rez_y)
 grd_z = np.linspace(lb[2], ub[2], rez_z)
 
-"""
+
 grid_vals = np.zeros([rez_x, rez_y, rez_z])
 for i in range(rez_x):
     print(f'{i}/{rez_x}')
     for j in range(rez_y):
         for k in range(rez_z):
             p = array([grd_x[i], grd_y[j], grd_z[k]])
-            grid_vals[i, j, k] = sum(residuals(p, multir_numba, lam, R))
+            grid_vals[i, j, k] = sum(residuals(p, multir_numba, lam, R)) + 0.1*(sum(p)/(3*ub[0]))*0.74421585
 
-np.save(f'{rez_x}_{rez_y}_{rez_z}_rez_xyz_cubed_grid-lb_ub_edges.npy', grid_vals)
+#np.save(f'{rez_x}_{rez_y}_{rez_z}_rez_xyz_cubed_grid-lb_ub_edges.npy', grid_vals)
 
-"""
-grid_vals = np.load('1000_1000_1000_rez_xyz_cubed_grid-lb_ub_edges.npy')
+#grid_vals = np.load('1000_1000_1000_rez_xyz_cubed_grid-lb_ub_edges.npy')
+
 #grid_vals = np.log10(grid_vals)
 
 fig = plt.figure()
--- results.py
+++ results.py
@@ -1,13 +1,16 @@
-from functions import plot
+from functions import plot, calc_loss
 import numpy as np
 from numpy import array
 from consts import um_to_m
 
 d_best = np.array([37.29533693, 626.64077655, 37.2953365])*um_to_m # from scipy.optimize (least_sq)
 p_brutef = array([205.2044088176353, 638.9178356713428, 797.797595190381])*um_to_m  # d2 > 100 um
-p_brutef_g = array([57.05611222444891, 39.75951903807616, 605.6052104208418])*um_to_m  # global minimum (2 um rez)
+p_full_search = array([57.05611222444891, 39.75951903807616, 605.6052104208418])*um_to_m  # global minimum (2 um rez)
 p_test = array([37.05611222444891, 626.6052104208418, 39.75951903807616])*um_to_m
 
+
 if __name__ == '__main__':
+    print(calc_loss(d_best))
+    print(calc_loss(p_full_search))
     #plot(p_brutef_g)
-    plot(d_best)
+    plot(p_full_search)
