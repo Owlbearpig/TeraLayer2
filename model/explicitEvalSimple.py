7968e88b0db46a27f25717376f60928af2fa8dc9
Owlbearpig <alexj07@gmail.com>
2021-12-15 01:31:08 +0100

    - ...

--- model/explicitEvalSimple.py
+++ model/explicitEvalSimple.py
@@ -3,6 +3,7 @@ import numpy as np
 from numba import jit
 from consts import *
 from results import d_best
+from optimization.nelderMeadSource import _minimize_neldermead
 from functions import format_data, calc_loss
 from model.multir_numba import multir_numba
 
@@ -13,28 +14,60 @@ f = array([13235.26131362, 16379.02884655, 20465.92663936,
 
 g = array([
     24705.82111877, 30574.18718023, 38203.06306014,
-    47005.61215233, 49939.79518306, 55808.16124453,
-])
+    47005.61215233, 49939.79518306, 55808.16124453])
 
 sine_sign = lambda x: 1 if (x % (2 * pi) < pi) else -1
+
+def sine(x):
+    #print(x)
+    x -= (x > pi) * (2 * pi)
+    #print(x)
+
+    B = 4 / pi
+    C = -4 / (pi * pi)
+
+    y = B * x + C * x * abs(x)
+    P = 0.225
+
+    return P * (y * abs(y) - y) + y
+
+def cose(x):
+    x += pi/2
+    x -= (x > pi)*(2*pi)
+
+    return sine(x)
 
 def explicit_reflectance(p):
     R = np.zeros(6)
     for i in range(6):
+        if i != 2:
+            continue
         f0 = f[i] * p[0]
         f1 = g[i] * p[1]
         f2 = f[i] * p[2]
 
-        s0, s1, s2, s3 = f2 + f1 + f0, f2 - f1 - f0, f2 + f1 - f0, -f2 + f1 - f0
+        s0, s1, s2, s3 = f2 + f1 + f0, f2 - f1 - f0, f2 + f1 - f0, - f2 + f1 - f0
 
-        cs0, cs1, cs2, cs3 = cos(s0), cos(s1), cos(s2), cos(s3)
-        ss0, ss1, ss2, ss3 = sin(s0), sin(s1), sin(s2), sin(s3)
+        print(s0, s1, s2, s3)
+        s0 = s0 % (2 * pi)
+        s1 = s1 % (2 * pi)
+        s2 = s2 % (2 * pi)
+        s3 = s3 % (2 * pi)
+        print(s0, s1, s2, s3)
+
+        cs0, cs1, cs2, cs3 = cose(s0), cose(s1), cose(s2), cose(s3)
+        ss0, ss1, ss2, ss3 = sine(s0), sine(s1), sine(s2), sine(s3)
+
+        #print(cs0, cs1, cs2, cs3)
+        #print(ss0, ss1, ss2, ss3, '\n')
 
         m_12_r = (1 - a * a) * b * (cs2 - cs1)
         m_22_r = (1 - a * a) * (cs0 - b * b * cs3)
 
         m_12_i = - 2 * a * (ss0 + b * b * ss3) + (a * a + 1) * b * (ss1 - ss2)
         m_22_i = (a * a + 1) * (ss0 + b * b * ss3) + 2 * a * b * (ss2 - ss1)
+
+        #print(m_12_r, m_22_r, m_12_i, m_22_i)
 
         R[i] = (m_12_r * m_12_r + m_12_i * m_12_i) / (m_22_r * m_22_r + m_22_i * m_22_i)
 
@@ -49,16 +82,27 @@ if __name__ == '__main__':
 
     lam, R0 = format_data(mask=mask, sample_file_idx=sample_idx)
 
-    p0 = np.array([500, 600, 25]) * um_to_m
+    p0 = np.array([35, 600, 35]) * um_to_m
     R_numba = multir_numba(lam, p0)
     R_explicit = explicit_reflectance(p0)
+    exit()
 
     #avg_runtime(multir_numba, lam, d_best)
     #avg_runtime(explicit_reflectance, d_best)
 
     print(R_numba)
     print(R_explicit)
+    print(R_numba-R_explicit)
     print(np.all(np.isclose(R_numba, R_explicit)))
 
-
     print(calc_loss(p0, mask=mask, sample_file_idx=sample_idx))
+
+    d0 = array([50, 600, 50]) * um_to_m
+    lb = d0 - array([50, 100, 50]) * um_to_m
+    hb = d0 + array([50, 100, 50]) * um_to_m
+
+    # avg_runtime(minimize, error, d0, bounds=list(zip(lb, hb)), method='Nelder-Mead')
+    error = lambda p: sum((explicit_reflectance(p) - R0)**2)
+    fval, x, iterations, fcalls = _minimize_neldermead(error, d0, bounds=(lb, hb), adaptive=False)
+
+    print(fval, x*um, iterations, fcalls)
