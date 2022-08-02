83dfad11df78c315e3b353fddccca9d0ada7454e
Owlbearpig <alexj07@gmail.com>
2022-08-03 00:32:56 +0200

    - The phase is haunting me...

--- data_evaluation/model/itsjustaphase/avg_phase.py
+++ data_evaluation/model/itsjustaphase/avg_phase.py
@@ -27,17 +27,35 @@ data_array = np.array([read_file(file) for file in file_paths if "Kopf" in str(f
 sam_idx = 55
 freqs = data_array[0, :, 0] * MHz
 
+f_slice = (0*THz < freqs) * (freqs < 1.45*THz)
+freqs = freqs[f_slice]
+
 plt.figure("Amplitude frequency domain")
-plt.plot(freqs, 20*np.log10(np.abs(ref_data[:, 1])), label="reference")
-plt.plot(freqs, 20*np.log10(np.abs(bg_data[:, 1])), label="background")
-plt.plot(freqs, 20*np.log10(np.abs(data_array[sam_idx, :, 1])), label=f"sample idx: {sam_idx}")
+plt.plot(freqs, 20*np.log10(np.abs(ref_data[f_slice, 1])), label="reference")
+plt.plot(freqs, 20*np.log10(np.abs(bg_data[f_slice, 1])), label="background")
+plt.plot(freqs, 20*np.log10(np.abs(data_array[sam_idx, f_slice, 1])), label=f"sample idx: {sam_idx}")
 plt.xlabel("Frequency (Hz)")
 plt.ylabel("Amplitude (dB)")
 
 plt.figure("Phase frequency domain")
-plt.plot(freqs, np.unwrap(ref_data[:, 2]), label=f"reference")
-plt.plot(freqs, np.unwrap(data_array[sam_idx, :, 2]), label=f"sample idx: {sam_idx}")
+diff = np.unwrap(ref_data[:, 2])[636]-np.unwrap(data_array[sam_idx, :, 2])[636]
+plt.plot(freqs, np.unwrap(ref_data[f_slice, 2])-diff, label=f"reference")
+plt.plot(freqs, np.unwrap(data_array[sam_idx, f_slice, 2]), label=f"sample idx: {sam_idx}")
 plt.xlabel("Frequency (Hz)")
 plt.ylabel("Phase (rad)")
+
+
+Y_sam = data_array[sam_idx, f_slice, 1]*np.exp(1j*data_array[sam_idx, f_slice, 2])
+Y_ref = ref_data[f_slice, 1]*np.exp(1j*ref_data[f_slice, 2]-1j*diff)
+
+y_sam = np.fft.ifft(Y_sam)
+y_ref = np.fft.ifft(Y_ref)
+
+
+plt.figure("Amplitude time domain")
+plt.plot(y_ref, label=f"reference")
+plt.plot(y_sam, label=f"sample idx: {sam_idx}")
+plt.xlabel("Time (?)")
+plt.ylabel("Amplitude (a.u.)")
 plt.legend()
 plt.show()
--- data_evaluation/model/itsjustaphase/simulation/signal_simulation.py
+++ data_evaluation/model/itsjustaphase/simulation/signal_simulation.py
@@ -4,12 +4,62 @@ Created on Wed Feb 10 16:00:13 2021
 
 @author: Talebf
 """
-
+from scipy import signal
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 from scipy.stats import norm
 
+THz = 10 ** 12
+
+def plot_freq_response(b, a, fs, worN=8000):
+    # Plot the frequency response.
+    w, h = signal.freqz(b, a, worN=worN)
+    plt.figure("Filter Frequency Response")
+    #plt.plot(0.5 * fs * w / (10 ** 12 * np.pi), 20 * np.log10(np.abs(h)), 'b')
+    plt.plot(0.5 * fs * w / np.pi, 20 * np.log10(np.abs(h)))
+    # plt.plot(highcut, 0.5*np.sqrt(2), 'ko')
+    # plt.axvline(highcut, color='k')
+    # plt.xlim(0, 0.5*fs / GHz)
+    plt.title("Filter Frequency Response")
+    plt.xlabel('Frequency (THz)')
+    plt.grid()
+
+
+def butter_highpass(data, lowcut, fs, order=7, plot=False):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    b, a = signal.butter(order, low, btype='high')
+
+    f = signal.filtfilt(b, a, data)
+    if plot:
+        plot_freq_response(b, a, fs)
+
+    return f
+
+def butter_lowpass(data, highcut, fs, order=7, plot=False):
+    #nyq = 0.5 * fs
+    #high = highcut / nyq
+    #print(highcut, nyq)
+    b, a = signal.butter(order, highcut, btype='low', fs=fs)
+
+    f = signal.lfilter(b, a, data)
+    if plot:
+        plot_freq_response(b, a, fs)
+
+    return f
+
+def butter_bandpass(data, lowcut, highcut, fs, order=7, plot=False):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = signal.butter(order, [low, high], btype='band')
+
+    f = signal.filtfilt(b, a, data)
+    if plot:
+        plot_freq_response(b, a, fs)
+
+    return f
 
 #######################################
 def thz_pulse(t, tau=0.15, loc=0):
@@ -20,11 +70,13 @@ def thz_pulse2(t, tau=0.15):
     return (t / tau ** 2) * (np.exp(-t ** 2 / tau ** 2))
 
 
-# tau = 0.3  # mm/ps
+# most similar to measurement values (?)
 tau = 0.4  # mm/ps
-#dt = 0.33  # Ps
 dt = 0.33  # Ps
-# dt = 0.01  # Ps
+
+#tau = 0.1  # mm/ps
+#dt = 0.01  # Ps
+
 t = np.arange(0, 1000, dt)
 t_r = 46.45
 t_0 = t_r + 0
@@ -80,19 +132,32 @@ plt.plot(freq[idx], 20 * np.log10(np.abs(Y2[idx])), label="Sample")
 plt.xlabel("Frequency (Hz)")
 plt.ylabel("Amplitude (dB)")
 
-np.unwrap = lambda x: x
+#np.unwrap = lambda x: x
 plt.figure("Phase frequency domain (Sim)")
 plt.plot(freq[idx], np.unwrap(np.angle(Y[idx])), label="Reference")
 plt.plot(freq[idx], np.unwrap(np.angle(Y2[idx])), label="Sample")
 plt.xlabel("Frequency (Hz)")
 plt.ylabel("Phase (rad)")
 
+
 y = np.fft.ifft(Y)
 y2 = np.fft.ifft(Y2)
+t_fun = np.fft.ifft(Y2/Y)
+t_fun = np.roll(t_fun, 1000)
+
+fs = 1 / dt
+
+lc, hc = 0.1, 1.5
+
+t_fun_filt = butter_lowpass(t_fun, hc, fs, order=1, plot=True)
+#t_fun_filt = butter_highpass(t_fun, lc, fs, order=2, plot=True)
+#t_fun_filt = butter_bandpass(t_fun, lc, hc, fs, order=1, plot=True)
 
 plt.figure("Amplitude time domain (Sim)")
-plt.plot(t, y, label="Reference")
-plt.plot(t, y2, label="Sample")
+plt.plot(t, (y), label="Reference")
+plt.plot(t, (y2), label="Sample")
+plt.plot(t, (t_fun), label="transfer func")
+plt.plot(t, (t_fun_filt), label="transfer func filtered")
 plt.legend()
 plt.show()
 
