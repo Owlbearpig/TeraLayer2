5d5448a9a9a7deae260be8981e8e7d042860e2bc
Owlbearpig <alexj07@gmail.com>
2023-02-28 22:39:43 +0100

    - OPBesteck images

--- data_evaluation/meas_eval/OP_Besteck/load_data.py
+++ data_evaluation/meas_eval/OP_Besteck/load_data.py
@@ -24,24 +24,62 @@ class OPMeasurement:
              "2023-22-02_Probe_blau_Metall_Lack_corrected.tim.npy"]
     files = [op_besteck_dir / file for file in files]
 
-    set1_info = "240, 121, 0.05, 0.05, 1, 1400, 0.050, 30000"
-    set2_info = "160, 201, 0.05, 0.05, 1, 1400, 0.050, 34000"
+    # set1_info = "240, 121, 0.05, 0.05, 1, 1400, 0.050, 30000"
+    set1_info = {"w": 240, "h": 121, "dx": 0.05, "dy": 0.05, "dt": 0.05, "samples": 1400}
+    # set2_info = "160, 201, 0.05, 0.05, 1, 1400, 0.050, 34000"
+    set2_info = {"w": 160, "h": 201, "dx": 0.05, "dy": 0.05, "dt": 0.05, "samples": 1400}
 
     def __init__(self, area_idx=0):
-        if area_idx == 0:
-            self.arr = np.load(self.files[0]).reshape((240, 121, 1400))
+        self.area_idx = area_idx
+        if self.area_idx == 0:
+            self.info = self.set1_info
+            self.arr = np.load(self.files[0]).reshape((self.info["w"], self.info["h"], self.info["samples"]))
         else:
-            self.arr = np.load(self.files[3]).reshape((160, 201, 1400))
+            self.info = self.set2_info
+            self.arr = np.load(self.files[3]).reshape((self.info["w"], self.info["h"], self.info["samples"]))
+
 
     def image(self, type_="p2p"):
+        info = self.info
         if type_ == "p2p":
-            img = np.max(np.abs(self.arr), axis=2)
+            grid_vals = np.max(np.abs(self.arr), axis=2)
         else:
-            img = np.argmax(np.abs(self.arr), axis=2)
-        plt.imshow(img[:, 50:150])
+            grid_vals = np.argmax(np.abs(self.arr), axis=2)
+
+        fig = plt.figure("Image")
+        ax = fig.add_subplot(111)
+        ax.set_title(f"Area {self.area_idx}")
+        fig.subplots_adjust(left=0.2)
+        extent = [0, info["w"] * info["dx"], 0, info["h"] * info["dy"]]
+        img = ax.imshow(grid_vals.transpose((1, 0)),
+                        vmin=np.min(grid_vals), vmax=np.max(grid_vals),
+                        origin="upper",
+                        cmap=plt.get_cmap('jet'),
+                        extent=extent)
+
+        ax.set_xlabel("x (mm)")
+        ax.set_ylabel("y (mm)")
+
+        cbar = fig.colorbar(img)
+        cbar.set_label(f"{type_}", rotation=270, labelpad=10)
+
+
+    def plot_point(self, x, y):
+        dx, dy, dt = self.info["dx"], self.info["dy"], self.info["dt"]
+        print(int(x/dx), int(3/dy))
+        y_td = self.arr[int(x/dx), int(y/dy)]
+        t = np.arange(0, dt*len(y_td), dt)
+
+        plt.figure("Single point")
+        #plt.plot(t, y_td, label=f"{x}, {y}")
+        plt.plot(y_td, label=f"{x}, {y}")
+        plt.xlabel("Time (ps)")
+        plt.ylabel("Amplitude (Arb. u.)")
+        plt.legend()
 
 
 if __name__ == '__main__':
-    measurement = OPMeasurement(area_idx=1)
+    measurement = OPMeasurement(area_idx=0)
     measurement.image(type_="p2p")
+    measurement.plot_point(x=6.0, y=5.0)
     plt.show()
