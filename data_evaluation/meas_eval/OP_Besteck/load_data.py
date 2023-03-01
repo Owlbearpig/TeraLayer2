662345c6bb55081046a91990feab5c0bdf11d316
Owlbearpig <alexj07@gmail.com>
2023-03-02 00:19:57 +0100

    - OPBesteck minor changes

--- data_evaluation/meas_eval/OP_Besteck/load_data.py
+++ data_evaluation/meas_eval/OP_Besteck/load_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 from consts import *
 import numpy as np
 import matplotlib.pyplot as plt
+from functions import filtering
 
 
 files = ["2023-02-17_Probe_blau_hinten_corrected.tim.csv",
@@ -28,6 +29,7 @@ class OPMeasurement:
     set1_info = {"w": 240, "h": 121, "dx": 0.05, "dy": 0.05, "dt": 0.05, "samples": 1400}
     # set2_info = "160, 201, 0.05, 0.05, 1, 1400, 0.050, 34000"
     set2_info = {"w": 160, "h": 201, "dx": 0.05, "dy": 0.05, "dt": 0.05, "samples": 1400}
+    plotted_ref = False
 
     def __init__(self, area_idx=0):
         self.area_idx = area_idx
@@ -37,6 +39,8 @@ class OPMeasurement:
         else:
             self.info = self.set2_info
             self.arr = np.load(self.files[3]).reshape((self.info["w"], self.info["h"], self.info["samples"]))
+
+        self.ref_td = np.load(self.files[1])
 
 
     def image(self, type_="p2p"):
@@ -66,20 +70,28 @@ class OPMeasurement:
 
     def plot_point(self, x, y):
         dx, dy, dt = self.info["dx"], self.info["dy"], self.info["dt"]
-        print(int(x/dx), int(3/dy))
-        y_td = self.arr[int(x/dx), int(y/dy)]
+        h = self.info["h"]
+
+        y_td = self.arr[int(x/dx), h - int(y/dy)]
         t = np.arange(0, dt*len(y_td), dt)
 
+        y_td = filtering(y_td, wn=(0.001, 9.999), filt_type="bandpass", order=5)
+
         plt.figure("Single point")
-        #plt.plot(t, y_td, label=f"{x}, {y}")
-        plt.plot(y_td, label=f"{x}, {y}")
+        if not self.plotted_ref:
+            plt.plot(self.ref_td[:, 0]-self.ref_td[0, 0], self.ref_td[:, 1], label="Reference")
+            self.plotted_ref = True
+
+        plt.plot(t, y_td, label=f"x={x} (mm), y={y} (mm)")
         plt.xlabel("Time (ps)")
         plt.ylabel("Amplitude (Arb. u.)")
         plt.legend()
 
 
 if __name__ == '__main__':
-    measurement = OPMeasurement(area_idx=0)
+    measurement = OPMeasurement(area_idx=1)
     measurement.image(type_="p2p")
-    measurement.plot_point(x=6.0, y=5.0)
+    measurement.plot_point(x=1.0, y=5.0)
+    measurement.plot_point(x=4.5, y=5.0)
+    measurement.plot_point(x=7.0, y=5.0)
     plt.show()
