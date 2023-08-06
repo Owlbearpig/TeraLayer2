2f3f2539ced79fbe8b91bcbd99d30de2b222edc8
Owlbearpig <alexj07@gmail.com>
2023-08-07 00:22:39 +0200

    - .;.;.

--- interface/conv_recv.py
+++ interface/conv_recv.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 # from bitstring import BitArray
+import binascii
+chunk = 8
 
 vals = []
-with open("test", "rb") as file:
+with open("dump", "rb") as file:
     #data_dump = file.read()
     #print(data_dump[0:20])
     #s = data_dump[0:20]
@@ -13,12 +15,22 @@ with open("test", "rb") as file:
     dump_len = len(data_dump)
     print(data_dump)
     print(len(str(data_dump).split(r"\x")))
-    for i in range(len(data_dump)//8):
-        data_slice = data_dump[8*i:8*(i+1)]
-        #print(data_slice)
-        val = int.from_bytes(data_slice, byteorder="little")
+    for i in range(len(data_dump)//chunk):
+        data_slices = binascii.hexlify(data_dump[i * chunk:(i + 1) * chunk]).decode()
+        print(data_slices)
+        s = [data_slices[i:i + 2] for i in range(0, len(data_slices), 2)]
+        #s = [c[1] + c[0] for c in s]
+        #print(s)
+        input_str = '0x' + "".join(list(reversed(s)))
+        #print(input_str)
+        #print(int(input_str, 16))
+        #input_str = '0x' + "".join(list(reversed(s)))
+        #print(input_str)
+
+        #print()
+        #val = int.from_bytes(data_slice, byteorder="little")
         #print(val)
-        vals.append(val)
+        #vals.append(val)
 
 #plt.plot(vals)
 #plt.show()
--- interface/rate_check.py
+++ interface/rate_check.py
@@ -9,55 +9,60 @@ HOST = "192.168.178.24"
 PORT = 1001
 c_ = 2 ** 6 * 2 * pi * 2 ** (-11)  # conversion factor
 
+
 def flip(s):
     s_ = [s[i:i + 2] for i in range(0, len(s), 2)]
 
     return "".join(list(reversed(s_)))
 
-y = []
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.connect((HOST, PORT))
-    t0 = time.time()
-    cntr = 0
 
-    buf_len = 8*1024
-    chunk = 8
-    cntr_sum = 0
-    while True:
-        # time.sleep(0.01)
-        buffer = sock.recv(buf_len)
-        # print(time.time() - t0)
+with open("dump", "wb") as file:
+    y = []
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((HOST, PORT))
+        t0 = time.time()
+        cntr = 0
+        cntr1 = 0
+        buf_len = 64
+        chunk = 8
+        cntr_sum = 0
+        while True:
+            # time.sleep(0.01)
+            buffer = sock.recv(buf_len)
+            # print(time.time() - t0)
+            print(buffer)
+            print(len(buffer))
+            file.write(buffer)
+            for j in range(len(buffer) // chunk):
+                hexdata = binascii.hexlify(buffer[j * chunk:(j + 1) * chunk]).decode()
+                print(hexdata)
+                s = [hexdata[i:i + 2] for i in range(0, len(hexdata), 2)]
+                # print(s)
+                input_str = '0x' + "".join(list(reversed(s)))
+                # print(input_str)
+                clk_cycle_cnt = int(input_str, 16)
+                # print(clk_cycle_cnt, clk_cycle_cnt/)
 
-        #print(len(buffer))
-        for j in range(len(buffer) // chunk):
-            hexdata = binascii.hexlify(buffer[j*chunk:(j+1)*chunk]).decode()
-            # print(hexdata)
-            s = [hexdata[i:i + 2] for i in range(0, len(hexdata), 2)]
-            # print(s)
-            input_str = '0x' + "".join(list(reversed(s)))
-            # print(input_str)
-            c = BitArray(hex=input_str)
-            c_bin = str(c.bin)
+                c = BitArray(hex=input_str)
+                c_bin = str(c.bin)
 
-            fpga_cntr, d0, d1, d2 = c_bin[-64:-48], c_bin[-48:-32], c_bin[-32:-16], c_bin[-16:]
-            print(cntr, fpga_cntr, int(fpga_cntr, 2))
-            d0, d1, d2 = c_ * int(d0, 2), c_ * int(d1, 2), c_ * int(d2, 2)
-            fpga_cntr = int(fpga_cntr, 2)
+                fpga_cntr, d0, d1, d2 = c_bin[-64:-48], c_bin[-48:-32], c_bin[-32:-16], c_bin[-16:]
+                # print(cntr, fpga_cntr, int(fpga_cntr, 2))
+                d0, d1, d2 = c_ * int(d0, 2), c_ * int(d1, 2), c_ * int(d2, 2)
+                fpga_cntr = int(fpga_cntr, 2)
 
-            # print(fpga_cntr, d0, d1, d2)
-            cntr += 1
-            cntr_sum += fpga_cntr
-            y.append(fpga_cntr)
-            if cntr > 10000:
-                #pass
-                plt.plot(y)
-                plt.show()
-            if (cntr % 100) == 0:
-                print("cntr", cntr)
-                print("rate", (time.time() - t0) / cntr)
-                # cntr = 0
-                print("cntr_sum", cntr_sum)
-                cntr_sum = 0
-                # print(time.time() - t0)
-
-
+                # print(fpga_cntr, d0, d1, d2)
+                cntr += 1
+                cntr_sum += fpga_cntr
+                y.append(fpga_cntr)
+                if cntr > 1000:
+                    pass
+                    # plt.plot(y)
+                    # plt.show()
+                if (cntr % 100) == 0:
+                    print("cntr", cntr)
+                    print("rate", (time.time() - t0) / cntr)
+                    # cntr = 0
+                    print("cntr_sum", cntr_sum)
+                    # cntr_sum = 0
+                    # print(time.time() - t0)
