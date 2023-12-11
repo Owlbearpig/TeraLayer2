d9fc31b702d57bde5453620acb5cb497e4baf743
Owlbearpig <alexj07@gmail.com>
2023-12-11 10:30:12 +0100

    - small changes to phasor_plot.py; not working yet.

--- data_evaluation/model/separation_idea/phasor_plot.py
+++ data_evaluation/model/separation_idea/phasor_plot.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 from triple_layer import *
 from matplotlib.widgets import Button, Slider
 
@@ -5,10 +7,8 @@ from matplotlib.widgets import Button, Slider
 def phasors(d1_, d2_, freq_idx_=0):
     phi0 = d1_ * kz_list[freq_idx_, 1]
     x_ = np.exp(1j * 2 * phi0)
-    print(2 * phi0)
     phi1 = d2_ * kz_list[freq_idx_, 2]
     y_ = np.exp(1j * 2 * phi1)
-    print(2 * phi1)
 
     p0_, p1_, p2_, p3_ = c0[freq_idx_], c1[freq_idx_] * x_, c2[freq_idx_] * y_, c4[freq_idx_] * x_ * y_
     p4_, p5_, p6_, p7_ = c3[freq_idx_], c5[freq_idx_] * x_, c6[freq_idx_] * y_, c7[freq_idx_] * x_ * y_
@@ -20,9 +20,15 @@ def update(val):
     d1_slider_val, d2_slider_val = d1_slider.val, d2_slider.val
     phasors_ = phasors(d1_slider_val, d2_slider_val)
     for i, line in enumerate(lines):
-        xdata, ydata = [0, np.angle(phasors_[i])], [0, np.abs(phasors_[i])]
-        line.set_ydata(ydata)
+        phi, R = np.angle(phasors_[i]), np.abs(phasors_[i])
+        if i in [0, 4]:
+            xdata, ydata = [0, phi], [0, R]
+        else:
+            R_prev, phi_prev = np.abs(phasors_[i - 1]), np.angle(phasors_[i - 1])
+            xdata, ydata = [phi_prev, phi], [R_prev, R]
+
         line.set_xdata(xdata)
+        line.set_ydata(ydata)
 
     fig.canvas.draw_idle()
 
@@ -39,13 +45,13 @@ size = min(width, height)
 fig = plt.figure(figsize=(size, size))
 ax = fig.add_axes([0.1, 0.1, 0.8, 0.8], polar=True)
 ax.set_rticks([0.5, 1, 1.5, 2])  # Less radial ticks
-ax.set_ylim((0, 1.0))
+ax.set_ylim((0, 0.35))
 plt.grid(True)
 
 ax.set_title("And there was much rejoicing!", fontsize=20)
 
-d1_init, d2_init = 100, 150
-
+d1_init, d2_init = d_truth[1:3]
+print(d1_init, d2_init)
 axd1 = fig.add_axes([0.1, 0.25, 0.0225, 0.63])
 d1_slider = Slider(
     ax=axd1,
@@ -69,9 +75,15 @@ d2_slider = Slider(
 phasors0 = phasors(d1_init, d2_init)
 bar_colors = 4 * ['blue'] + 4 * ['red']
 lines = []
+print(phasors0)
 for i, p_ in enumerate(phasors0):
     R, phi = np.abs(p_), np.angle(p_)
-    line, = ax.plot([0, phi], [0, R], c=bar_colors[i])
+    if i in [0, 4]:
+        line, = ax.plot([0, phi], [0, R], c=bar_colors[i])
+    else:
+        R_prev, phi_prev = np.abs(phasors0[i - 1]), np.angle(phasors0[i - 1])
+        line, = ax.plot([phi_prev, phi], [R_prev, R], c=bar_colors[i])
+
     lines.append(line)
 
 # register the update function with each slider
@@ -111,4 +123,9 @@ for i, p_ in enumerate(phasors_):
     # plt_arrow(0, phi, 0, 1)
 """
 
+plt.figure()
+plt.plot([0, np.sum(phasors0[:4]).real], [0, np.sum(phasors0[:4]).imag], label="first 4")
+plt.plot([0, np.sum(phasors0[4:]).real], [0, np.sum(phasors0[4:]).imag], label="last 4")
+plt.xlabel("real")
+plt.ylabel("imag")
 show()
